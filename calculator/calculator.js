af49fb25132c2a089da9dcd1ca392254711cd591
jfitzg7 <jackf8088@gmail.com>
2022-07-14 01:57:14 -0600

    Added display, and implemented the clear button

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -20,3 +20,11 @@ function operate(operator, operand1, operand2) {
   else if (operator === "*") return multiply(operand1, operand2);
   else if (operator === "/") return divide(operand1, operand2);
 }
+
+function clearDisplay() {
+  const display = document.querySelector("#calculator-display");
+  display.textContent = "";
+}
+
+const clearBtn = document.querySelector("#pad-clear");
+clearBtn.addEventListener("click", clearDisplay);
